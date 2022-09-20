93d76876b69fd41d6069716cd40d2b11c9bf26ea
markos <markos.paltser@voco.ee>
2022-09-20 11:18:44 +0300

    Variables

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hello world")
+// variables
+// let
+let name = 'Markos Paltser'
+name = 'new name'
+
+//const
+
+const surname = 'Paltser'
+// surname = 'puusepp' - error
+
+//arrays
+
+const numbers = [1, 2, 3, 4, 5]
+console.log(numbers)
+numbers.push(6)   // push lisab l6ppu
+console.log(numbers)
+
+
+//object
+const person = {
+    firstname : 'Markos',
+    surname : 'Paltser'
+}
+console.log(person)
+console.log(person.firstname)
+console.log(person.surname)
+console.log(person)
