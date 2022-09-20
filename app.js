575da6651ed42fc013b4ecc4271f3a35faa78168
markos <markos.paltser@voco.ee>
2022-09-20 11:29:02 +0300

    Data type

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
-// variables
-// let
+// data type
+// string
 let name = 'Markos Paltser'
 name = 'new name'
 
-//const
+let goodMark = 5
+console.log(goodMark)
+console.log(typeof goodMark)
 
-const surname = 'Paltser'
-// surname = 'puusepp' - error
+let persent = 50.3
+console.log(persent)
+console.log(typeof persent)
 
-//arrays
+let isHere = true
+console.log(isHere)
+console.log(typeof isHere)
+
 
 const numbers = [1, 2, 3, 4, 5]
 console.log(numbers)
-numbers.push(6)   // push lisab l6ppu
-console.log(numbers)
+console.log(typeof numbers)
 
+//null
+const airplane = null
+console.log(airplane)
+console.log(typeof airplane)
 
-//object
-const person = {
-    firstname : 'Markos',
-    surname : 'Paltser'
-}
-console.log(person)
-console.log(person.firstname)
-console.log(person.surname)
-console.log(person)
+//symbol
+const sym = Symbol()
+console.log(sym)
+console.log(typeof sym)
+
+//undefined
+let test
+console.log(test)
+console.log(typeof test)
