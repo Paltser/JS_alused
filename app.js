653700025359f2bea05cc6f5ab0429625002daf8
markos <markos.paltser@voco.ee>
2022-09-22 13:43:36 +0300

    tingimuslaused

--- app.js
+++ app.js
@@ -1,21 +1,75 @@
-const today = new Date()
+/*
+if(conditional) {
+    // if statments
+} else {
+    //else statments
+}
 
-let val
+== equal     ->   ===
+!= not equal ->  !==
+ */
 
-val = today
-val = today.getMonth() // 0 - jaanuar
-val = today.getDate()
-val = today.getDay()
-val = today.getFullYear()
-val = today.getHours()
-val = today.getMinutes()
-val = today.getTime()
-val = today.getTimezoneOffset()
 
-val = new Date('09/22/2021')
-val = new Date('September 23 2022')
-val = new Date()
-val.setDate(23)
-val.setFullYear(2023)
+let personID;
 
-console.log(val)
+if (typeof personID !== 'indefined'){
+    console.log('id is not correct')
+} else {
+    console.log('id is ${personID}')
+}
+
+console.log(personID)
+
+/*
+if(conditional) {
+    // if statments
+} else if(conditional){
+    //if1 statments
+} else {
+    else statment
+}
+ */
+
+// const color = 'green'
+//
+// if (color === 'red'){
+//     console.log('stop!')
+// } else if (color === 'yellow') {
+//     console.log('attention!')
+// } else if (color === 'green') {
+//         console.log('go!')
+// } else {
+//     console.log('error')
+// }
+
+const name = 'kadi'
+const age = 40
+
+if (age > 0 && age <= 12 ){
+    console.log('laps')
+}else if (age > 12 && age < 18){
+    console.log('teen')
+}else {
+    console.log('t2iskasvanud')
+}
+
+let id = 15
+
+console.log(id === 100 ? 'ok' :'not ok' )
+
+
+//swithc
+const color = 'blue'
+switch (color) {
+    case "red":
+        console.log('stop')
+        break;
+    case "yellow":
+        console.log('attention')
+        break;
+    case "green":
+        console.log('go')
+        break;
+    default:
+        console.log('error')
+}
