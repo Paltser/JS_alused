cda6a0fc59da95dc4b7f4f45e5bd336e077becdc
markos <markos.paltser@voco.ee>
2022-09-26 13:59:31 +0300

    Dom elements

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-function greeting(name){
-    // console.log(`hello, ${name}!`)
-    return `hello, ${name}!`
-}
-// greeting('markos')
-let greet = greeting('markos')
-console.log(greet)
+let val;
+val = document
+val = document.all
+val = document.all[6]
+val = document.all.length
 
-const square = function (number) {
-    return number * number
-}
-let result = square(3)
-console.log(result)
+val = document.head
+val = document.body
 
-const todo = {
-    add: function () {
-        console.log('add dodo..')
-    },
-    edit: function (){
-        console.log('edit')
-    delete function () {
-            console.log("delete")
-    }
-    }
-}
+val = document.doctype
+val = document.domain
+val = document.URL
+val = document.characterSet
+val = document.contentType
+
+val = document.forms[0].method
+val = document.forms[0].action
+val = document.forms[0].id
+
+val = document.links
+val = document.links[1]
+val = document.links[1].id
+val = document.links[1].className
+val = document.links[1].classList
+val = document.links[1].classList[2]
+
+val = document.scripts[1]
+val = document.scripts[0]
+val = document.scripts[1].getAttribute('src')
+
+
+console.log(val)
