ee7a08ca58c430270b61e02173770493c15776b5
markos <markos.paltser@voco.ee>
2022-09-27 09:24:38 +0300

    Dom selecoter for single and mutliple elements

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 let val;
-val = document
-val = document.all
-val = document.all[6]
-val = document.all.length
-
-val = document.head
-val = document.body
-
-val = document.doctype
-val = document.domain
-val = document.URL
-val = document.characterSet
-val = document.contentType
-
-val = document.forms[0].method
-val = document.forms[0].action
-val = document.forms[0].id
-
-val = document.links
-val = document.links[1]
-val = document.links[1].id
-val = document.links[1].className
-val = document.links[1].classList
-val = document.links[1].classList[2]
-
-val = document.scripts[1]
-val = document.scripts[0]
-val = document.scripts[1].getAttribute('src')
 
 
+val = document.getElementById('task')
+
+val = document.getElementById
+('task').id
+val = document.getElementById
+('task').className
+
+const taskTitle = document.getElementById('task-title')
+
+// stiilitd
+taskTitle.style.background = '#333'
+taskTitle.style.color = '#fff'
+taskTitle.style.padding = '5px'
+taskTitle.style.display = ''
+
+//content
+taskTitle.textContent = 'defined tasks'
+taskTitle.innerText = 'my fav text'
+taskTitle.innerHTML = `<b style="color: red">My Tasks</b'>`
+
+val = document.querySelector('#task-title')
+val = document.querySelector('ul')
+
+val = document.querySelector('li')
+
+val = document.querySelector('li')
+
+// const task = document.querySelector('li')
+// task.style.color = 'green'
+
+// val = document.querySelector('li:last-child')
+// val = document.querySelector('li:nth-child(odd)').style.background = '#ccc'
+// val = document.querySelector('li:nth-child(even)').style.background = '#aaa'
+
+// multiple elements
+
+val = document.querySelectorAll('li')
+oddli = document.querySelectorAll('li:nth-child(odd)')
+
+oddli.forEach((li) =>{
+    li.style.background = '#ddd'
+})
+
+console.log(task)
 console.log(val)
